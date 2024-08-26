985510c7d63f9b31cfd095c0b7f4e0161d812d03
vedina <jeliazkova.nina@gmail.com>
2024-08-26 14:09:46 +0300

    metadata for the multidimensional case

--- examples/pipeline/convert2nexus.py
+++ examples/pipeline/convert2nexus.py
@@ -18,6 +18,7 @@ from pyambit.nexus_spectra import configure_papp
 import uuid
 import nexusformat.nexus.tree as nx
 import json, pickle
+import traceback
 
 def read_template(template_path):
     
@@ -31,6 +32,19 @@ def read_template(template_path):
     df_meta = pd.read_excel(template_path, sheet_name='Front sheet', skiprows=4)
     df_meta.dropna(axis=1,how='all',inplace=True)
     return df,df_meta
+
+# Function to convert columns to JSON-serializable format
+def get_unique_values(df):
+    # Initialize dictionaries
+    single_value_dict = {}
+    multi_value_dict = {}
+    for col in df.columns:
+        unique_values = df[col].unique()
+        if len(unique_values) == 1:
+            single_value_dict[col] = str(unique_values[0]).strip()
+        else:
+            multi_value_dict[col] = ', '.join(map(str, unique_values.tolist()))
+    return single_value_dict,multi_value_dict
 
 def process_files(root_folder,df,meta,multidimensional=False):
     substances = []
@@ -70,101 +84,118 @@ def process_files(root_folder,df,meta,multidimensional=False):
             for index, row in df_subfolder.iterrows():
                 try:
                     spe = rc2.spectrum.from_local_file(os.path.join(root_folder,instrument_keys[0],row["Basename"]).strip())
-                    spectra.append({"spe" : spe, "minx" : min(spe.x), "maxx" : max(spe.x), "bins" : len(spe.x), 
-                                    "replicate" : row[replicate], "meta" : spe.meta, "basename" : row["Basename"]})
+                    _sperow = {"spe" : spe, "minx" : min(spe.x), "maxx" : max(spe.x), "bins" : len(spe.x), 
+                                    "replicate" : row[replicate],  "basename" : row["Basename"], "@signal" : "Raman intensity", "@axes" : "Raman shift"}
+                    
+                    for key in spe.meta.get_all_keys():
+                        if key=="@signal":
+                            _sperow[key] =  "Raman intensity" if spe.meta[key] == "" else spe.meta[key]
+                        elif key=="@axes":
+                            _sperow[key] =  "Raman shift" if spe.meta[key][0] == "" else spe.meta[key][0]     
+                        else:                      
+                            _sperow["META_{}".format(key)] = spe.meta[key]
+                    spectra.append(_sperow)
                     
                 except Exception as err:
+                    traceback.print_exc()
                     print(row["Filename"],err)
 
             df_spectra = pd.DataFrame(spectra) 
-
-            grouped = df_spectra.groupby(['minx', 'maxx', 'bins'])
+            df_spectra_META = df_spectra[[col for col in df_spectra.columns if col.startswith('META_')]]
+            
+            _conditions_single, _conditions_multi = get_unique_values(df_spectra_META)
+                    
+            grouped = df_spectra.groupby(['minx', 'maxx', 'bins','@signal','@axes'])
             for group_keys, group_df in grouped:
                 #print(row[replicate],sample,subfolder,row["Basename"],group_keys)
                 #print(f"Group keys (minx, maxx, bins): {group_keys}")
                 #print(group_df)
                 #print("\n")
-                
-                unique_replicates = sorted(group_df['replicate'].unique())
-                num_replicates = len(unique_replicates)
-                if multidimensional:                
-                    array_2d = np.empty((num_replicates,group_keys[2]))
-                    for i, (index, row) in enumerate(group_df.iterrows()):
-                        spe_y = row['spe'].y
-                        spe_x = row['spe'].x
-                        replicate_number = row['replicate']
-                        # Fill the ith column with the spe.x values of the current replicate
-                        column_index = unique_replicates.index(replicate_number)
-                        array_2d[column_index,:] = spe_y
-                        
-                    #print(type(array_2d),array_2d)
-                    data_dict: Dict[str, mx.ValueArray] = {
-                        "Replicate": mx.ValueArray(values=np.array(unique_replicates), unit=None),
-                        "Raman shift": mx.ValueArray(values=spe_x, unit="cm-1")
-                    }   
-                    ea = mx.EffectArray(
-                            endpoint="Raman intensity",
-                            endpointtype="RAW_DATA",
-                            signal=mx.ValueArray(values=array_2d, unit="a.u."),
-                            axes=data_dict,
-                            #conditions={replicate : row[replicate]} # , "Original file" : meta["Original file"]}
-                            conditions={ "grouped_by" : ', '.join(map(str, group_keys)) }
-                    )
-                    print(ea.nx_name )
-                    ea.nx_name = f'{sample} {subfolder}'
-                    papp.effects.append(ea)      
-                else:
-                    signal = None
-                    signal_name = None
-                    auxiliary= {}
-                    data_dict: Dict[str, mx.ValueArray] = {}
-                    
-                    for i, (index, row) in enumerate(group_df.iterrows()):
-                        spe_y = row['spe'].y
-                        spe_x = row['spe'].x
-
-                        _name = "Raman intensity"
-                        _axes = ["Raman shift"]
-                        _signal_meta = None
-                        _spemeta = None
-                        for key in row['spe'].meta.get_all_keys():
-                            if key == "@signal":
-                                _name = row['spe'].meta[key]
-                            elif key == "@axes":
-                                _axes = row['spe'].meta[key]
-                            else:
-                                if _spemeta is None:
-                                    _spemeta = {}
-                                _spemeta[f'META_{key}'] = str(row['spe'].meta[key])
-                        #_name = row['spe'].meta["@signal"] if "@signal" in row['spe'].meta.get_all_keys() else "Raman intensity"
-                        #_axes = row['spe'].meta["@axes"] if "@axes" in row['spe'].meta.get_all_keys() else ["Raman shift"]
-
-                        replicate_number = row['replicate']
-                        if signal is None:
+                try:
+                    unique_replicates = sorted(group_df['replicate'].unique())
+                    num_replicates = len(unique_replicates)
+                    if multidimensional:                
+                        array_2d = np.empty((num_replicates,group_keys[2]))
+                        for i, (index, row) in enumerate(group_df.iterrows()):
+                            spe_y = row['spe'].y
+                            spe_x = row['spe'].x
+                            replicate_number = row['replicate']
+                            # Fill the ith column with the spe.x values of the current replicate
+                            column_index = unique_replicates.index(replicate_number)
+                            array_2d[column_index,:] = spe_y
                             
-                            signal_endpointtype = "RAW_DATA" if _name == "" else _name
-                            signal = spe_y
-                            if _spemeta is None:
-                                _spemeta={}
-                            _signal_meta = _spemeta
-                            _signal_meta["REPLICATE"] = str(replicate_number)
-                            data_dict : Dict[str, mx.ValueArray] = { "Raman shift": mx.ValueArray(values=spe_x, unit="1/cm")}
-                        else:
-                            _spemeta["REPLICATE"] = str(replicate_number)
-                            #auxiliary["{}".format(row["basename"])] = mx.MetaValueArray(values=spe_y,unit="a.u",conditions=_spemeta)
-                            auxiliary["Raman intensity ({})".format(row["replicate"])] = mx.MetaValueArray(values=spe_y,unit="a.u",conditions=_spemeta)
-                    #print(papp.uuid,auxiliary)                           
-                    ea = mx.EffectArray(
-                            endpoint="Raman intensity",
-                            endpointtype=signal_endpointtype,
-                            signal=mx.ValueArray(values=signal, unit="a.u.",auxiliary = auxiliary,conditions=_signal_meta),
-                            axes=data_dict,
-                            #conditions={replicate : row[replicate]} # , "Original file" : meta["Original file"]}
-                            conditions={ "grouped_by" : ', '.join(map(str, group_keys)) }
-                    )
-                    ea.nx_name = f'{sample} {subfolder}'
-                    papp.effects.append(ea)
-   
+                        #print(type(array_2d),array_2d)
+                        data_dict: Dict[str, mx.ValueArray] = {
+                            "Replicate": mx.ValueArray(values=np.array(unique_replicates), unit=None),
+                            "Raman shift": mx.ValueArray(values=spe_x, unit="cm-1")
+                        }   
+
+                        _conditions_single["grouped_by"] = ', '.join(map(str, group_keys)) 
+                        ea = mx.EffectArray(
+                                endpoint="Raman intensity",
+                                endpointtype=group_keys[3],
+                                signal=mx.ValueArray(values=array_2d, unit="a.u.",conditions = None if _conditions_multi == {} else _conditions_multi),
+                                axes=data_dict,
+                                #conditions={replicate : row[replicate]} # , "Original file" : meta["Original file"]}
+                                conditions= _conditions_single
+                        )
+                        ea.nx_name = f'{sample} {subfolder}'
+                        papp.effects.append(ea)      
+                    else:
+                        signal = None
+                        signal_name = None
+                        auxiliary= {}
+                        data_dict: Dict[str, mx.ValueArray] = {}
+                        
+                        for i, (index, row) in enumerate(group_df.iterrows()):
+                            spe_y = row['spe'].y
+                            spe_x = row['spe'].x
+
+                            _name = "Raman intensity"
+                            _axes = ["Raman shift"]
+                            _signal_meta = None
+                            _spemeta = None
+                            for key in row['spe'].meta.get_all_keys():
+                                if key == "@signal":
+                                    _name = row['spe'].meta[key]
+                                elif key == "@axes":
+                                    _axes = row['spe'].meta[key]
+                                else:
+                                    if _spemeta is None:
+                                        _spemeta = {}
+                                    _spemeta[f'META_{key}'] = str(row['spe'].meta[key])
+                            #_name = row['spe'].meta["@signal"] if "@signal" in row['spe'].meta.get_all_keys() else "Raman intensity"
+                            #_axes = row['spe'].meta["@axes"] if "@axes" in row['spe'].meta.get_all_keys() else ["Raman shift"]
+
+                            replicate_number = row['replicate']
+                            if signal is None:
+                                
+                                signal_endpointtype = "RAW_DATA" if _name == "" else _name
+                                signal = spe_y
+                                if _spemeta is None:
+                                    _spemeta={}
+                                _signal_meta = _spemeta
+                                _signal_meta["REPLICATE"] = str(replicate_number)
+                                data_dict : Dict[str, mx.ValueArray] = { "Raman shift": mx.ValueArray(values=spe_x, unit="1/cm")}
+                            else:
+                                _spemeta["REPLICATE"] = str(replicate_number)
+                                #auxiliary["{}".format(row["basename"])] = mx.MetaValueArray(values=spe_y,unit="a.u",conditions=_spemeta)
+                                auxiliary["Raman intensity ({})".format(row["replicate"])] = mx.MetaValueArray(values=spe_y,unit="a.u",conditions=_spemeta)
+                        #print(papp.uuid,auxiliary)                           
+                        ea = mx.EffectArray(
+                                endpoint="Raman intensity",
+                                endpointtype=signal_endpointtype,
+                                signal=mx.ValueArray(values=signal, unit="a.u.",auxiliary = auxiliary,conditions=_signal_meta),
+                                axes=data_dict,
+                                #conditions={replicate : row[replicate]} # , "Original file" : meta["Original file"]}
+                                conditions={ "grouped_by" : ', '.join(map(str, group_keys)) }
+                        )
+                        ea.nx_name = f'{sample} {subfolder}'
+                        papp.effects.append(ea)
+                except Exception as e:
+                    traceback.print_exc()
+                    print(e,group_keys)
+
                 
             substance.study.append(papp)
         substances.append(substance)       
@@ -219,4 +250,5 @@ try:
 
     nxroot.save(file, mode="w") 
 except Exception as err:
+    traceback.print_exc()
     print(err)       
